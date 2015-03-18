21c01ff50296ac1eab5d61d56c573556b0ab35f5
josephlin0408 <fun.josephlin@gmail.com>
2015-03-18 17:13:00 +0800

    products_controller 有bug

--- app/controllers/seller/products_controller.rb
+++ app/controllers/seller/products_controller.rb
@@ -1,2 +1,23 @@
 class Seller::ProductsController < Seller::BaseController
+	def index
+		@products = StoreProduct.all
+	end
+
+	def new
+		@product = StoreProduct.new
+	end
+
+	def create
+		@product = StoreProduct.new(product_params)
+		if @product.save
+			redirect_to seller_products_path
+		else
+			render :new
+		end
+	end
+
+	private
+	def product_params
+		params.require(:store_product).permit(:name, :store_id, :origin_price, :price, :size, :image, :content)
+	end
 end
